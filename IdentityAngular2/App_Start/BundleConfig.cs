6d0c5ad5a72aefbd59acde4f8dbb915070c2dd9d
SIMFORM\Mustansir.Sabir <mustansir.s@simformsolutions.com>
2017-09-12 11:08:01 +0530

    Added Theme for the project

--- IdentityAngular2/App_Start/BundleConfig.cs
+++ IdentityAngular2/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace IdentityAngular2
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            #region Default bundle
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Assets/scripts/jquery-{version}.js"));
 
@@ -26,6 +28,40 @@ namespace IdentityAngular2
             bundles.Add(new StyleBundle("~/Assets/css/content/css").Include(
                       "~/Assets/css/content/bootstrap.css",
                       "~/Assets/css/content/site.css"));
+
+            #endregion
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            "~/Assets/scripts/jquery/jquery.unobtrusive*",
+            "~/Assets/scripts/jquery/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundle/theme/jquery").Include(
+                      "~/Assets/scripts/jquery/jquery-{version}.js",
+                      "~/Assets/scripts/bootstrap.min.js",
+                      "~/Assets/scripts/theme.js",
+                      "~/Assets/scripts/bootstrap.datepicker.js",
+                      "~/Assets/plugins/toastr/toastr.min.js",
+                      "~/Assets/scripts/select2.min.js"));
+
+            bundles.Add(new StyleBundle("~/bundle/theme/bootstrap").Include(
+                      "~/Assets/theme/css/bootstrap/bootstrap.css",
+                      "~/Assets/theme/css/bootstrap/bootstrap-responsive.css",
+                      "~/Assets/theme/css/bootstrap/bootstrap-overrides.css"));
+
+            bundles.Add(new StyleBundle("~/bundle/theme/css").Include(
+                      "~/Assets/theme/css/layout.css",
+                      "~/Assets/theme/css/elements.css",
+                      "~/Assets/theme/css/icons.css",
+                      "~/Assets/theme/css/compiled/form-showcase.css",
+                      "~/Assets/theme/css/lib/font-awesome.css",
+                      "~/Assets/theme/css/compiled/tables.css",
+                      "~/Assets/theme/css/compiled/calendar.css",
+                      "~/Assets/theme/css/lib/bootstrap.datepicker.css",
+                      "~/Assets/theme/css/googleFonts-OpenSans.css",
+                      "~/Assets/theme/css/googleFonts-Lato.css",
+                      "~/Assets/plugins/toastr/toastr.css",
+                      "~/Assets/theme/css/lib/select2.css"));
+
         }
     }
 }
---
+++ IdentityAngular2/Assets/scripts/custom/CustomPopup.js
@@ -0,0 +1,123 @@
+﻿
+// Author : Mustansir Sabir
+// Usage  : Popup Script for Messagebox.
+function MsgBox(title, message) {
+
+    //$("body").append("<div id='CustomModalPopup' class='modal fade in' style='display: block;' aria-hidden='false'>" +
+    //                    "<div class='modal-backdrop fade in' style='height: 100%;opacity: 0.1 !important;'></div>" +
+    //                    "<div class='modal-dialog'>" +
+    //                        "<div class='modal-content'>" +
+    //                            "<div class='modal-header'>" +
+    //                                "<button aria-hidden='true' data-dismiss='modal' class='close' type='button' onClick='CloseMsgBoxWithFalse();'>×</button>" +
+    //                                "<h4 class='modal-title'>" + String(title) + "</h4>" +
+    //                            "</div>" +
+    //                            "<div class='modal-body'>" + String(message) + "</div>" +
+    //                            "<div class='modal-footer'>" +
+    //                            "<button aria-hidden='true' data-dismiss='modal' class='btn btn-info' type='button' onClick='CloseMsgBoxWithFalse();'>OK</button>" +
+    //                        "</div>" +
+    //                    "</div>" +
+    //                "</div>" +
+    //            "</div>");
+
+    $("body").append("<div id='CustomModalPopup' class='popup-wrapper'>" +
+                    "<div class='popup-overlay'></div>" +
+                    "<div class='new-event popup popup-small'>" +
+                        "<i class='close-pop table-delete' onClick='CloseMsgBoxWithFalse();'></i>" +
+                        "<h5>" + title + "</h5>" +
+                        "<div class='row-fluid form-wrapper'>" +
+                            "<div class='span12 field-box m-b-0'>" +
+                                "<label class='span12'>" + message + "</label>" +
+                            "</div>" +
+                            "<div class='span11 field-box actions'>" +
+                                "<input type='button' value='Ok' class='btn-glow primary' onClick='CloseMsgBoxWithFalse();'>" +
+                            "</div>" +
+                        "</div>" +
+                    "</div>" +
+                "</div>");
+
+    $('#CustomModalPopup').modal('show', { backdrop: 'fade' });
+}
+
+// Author : Mustansir Sabir
+// Usage  : Popup Script for DeleteConfirmationBox
+function MsgBoxConfirmGrid(title, message, controlId) {
+
+    $("body").append("<div id='CustomModalPopup' class='popup-wrapper'>" +
+                        "<div class='popup-overlay'></div>" +
+                        "<div class='new-event popup popup-small'>" +
+                            "<i class='close-pop table-delete' onClick='CloseMsgBoxWithFalse(" + controlId + ");'></i>" +
+                            "<h5>" + title + "</h5>" +
+                            "<div class='row-fluid form-wrapper'>" +
+                                "<div class='span12 field-box m-b-0'>" +
+                                    "<label class='span12'>" + message + "</label>" +
+                                "</div>" +
+                                "<div class='span11 field-box actions'>" +
+                                    "<input type='button' value='Yes' class='btn-glow primary' onClick='CloseMsgBoxWithTrue(" + controlId + ");'>" +
+                                    "<input type='reset' class='reset' value='No' onClick='CloseMsgBoxWithFalse();'>" +
+                                "</div>" +
+                            "</div>" +
+                        "</div>" +
+                    "</div>");
+    return false;
+}
+
+function CloseMsgBoxWithFalse() {
+    $('#CustomModalPopup').modal('hide', { backdrop: 'fade' });
+    $("#CustomModalPopup").remove();
+    $("body").removeClass("modal-open");
+    $("body").attr("style", "padding-right: 0px");
+    return false;
+}
+
+function CloseMsgBoxWithTrue(controlId) {
+    $(controlId).next().click();
+    $("#CustomModalPopup").remove();
+    $("body").removeClass("modal-open");
+    $("body").attr("style", "padding-right: 0px");
+    return true;
+}
+
+function showreminder() {
+
+    if ($('#CPH_chkbxReminder').prop('checked') == true) {
+        $('#CPH_txtreminder').prop('disabled', false);
+        $('#CPH_txtremindertime').prop('disabled', false)
+    }
+    else {
+        $('#CPH_txtreminder').prop('disabled', true);
+        $('#CPH_txtremindertime').prop('disabled', true)
+
+    }
+}
+
+
+// Author : Mustansir Sabir
+// Usage  : Popup Script for custom button text
+function MsgBoxConfirmGridCustomButtons(title, message, controlId, confirmText, cancelText) {
+
+    $("body").append("<div id='CustomModalPopup' class='popup-wrapper'>" +
+                        "<div class='popup-overlay'></div>" +
+                        "<div class='new-event popup popup-small'>" +
+                            "<i class='close-pop table-delete' onClick='CloseMsgBoxWithFalse(" + controlId + ");'></i>" +
+                            "<h5>" + title + "</h5>" +
+                            "<div class='row-fluid form-wrapper'>" +
+                                "<div class='span12 field-box m-b-0'>" +
+                                    "<label class='span12'>" + message + "</label>" +
+                                "</div>" +
+                                "<div class='span11 field-box actions'>" +
+                                    "<input type='button' value='" + confirmText + "' class='btn-glow primary' onClick='CloseMsgBoxWithTrueSameControl(" + controlId + ");'>" +
+                                    "<input type='reset' class='reset' value='" + cancelText + "' onClick='CloseMsgBoxWithFalse();'>" +
+                                "</div>" +
+                            "</div>" +
+                        "</div>" +
+                    "</div>");
+    return false;
+}
+
+function CloseMsgBoxWithTrueSameControl(controlId) {
+    $(controlId).click();
+    $("#CustomModalPopup").remove();
+    $("body").removeClass("modal-open");
+    $("body").attr("style", "padding-right: 0px");
+    return true;
+}
